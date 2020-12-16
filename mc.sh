765e9e20308547b9d8dc32c93f2ea1d904edc97f
raiderman1 <47142963+jasonmichael13@users.noreply.github.com>
2020-12-16 22:29:33 +0200

    Create mc.sh

---
+++ mc.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/AppImages
+wget https://mcpelauncher.mrarm.io/appimage/Minecraft_Bedrock_Launcher.AppImage -O ~/AppImages/Minecraft_Bedrock_Launcher.AppImage
+chmod +x ~/AppImages/Minecraft_Bedrock_Launcher.AppImage
+cd ~/AppImages/
+./Minecraft_Bedrock_Launcher.AppImage
